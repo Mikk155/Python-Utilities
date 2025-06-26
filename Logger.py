506a4135c00d43abdd4dd18c99eedcc28571ff34
Mikk155 <martimsanchez155@gmail.com>
2025-06-26 19:15:26 -0300

    Update logger level selection

--- Logger.py
+++ Logger.py
@@ -2,45 +2,55 @@ from colorama import init as colorama
 global colorama_init;
 colorama_init: bool = False;
 
+class LoggerLevel:
+
+    AllLoggers = -1;
+    Critical: int = ( 1 << 0 ); # critical is ignored and will always being enable
+    Error: int = ( 1 << 1 ); # The same for error
+    Warning: int = ( 1 << 2 );
+    Information: int = ( 1 << 3 );
+    Debug: int = ( 1 << 4 );
+    Trace: int = ( 1 << 5 );
+
+global GlobalLoggerLevel;
+GlobalLoggerLevel = ( LoggerLevel.Information | LoggerLevel.Warning );
+
+def LoggerToggleLevel( level: LoggerLevel ):
+    global GlobalLoggerLevel;
+    if GlobalLoggerLevel & level:
+        GlobalLoggerLevel &= level;
+    else:
+        GlobalLoggerLevel |= level;
+
+def LoggerSetLevel( level: LoggerLevel ):
+    global GlobalLoggerLevel;
+    if not GlobalLoggerLevel & level:
+        GlobalLoggerLevel |= level;
+
+def LoggerClearLevel( level: LoggerLevel ):
+    global GlobalLoggerLevel;
+    if GlobalLoggerLevel & level:
+        GlobalLoggerLevel &= level;
+
 class Logger():
 
     name: str;
 
-    class Level:
+    level: int = LoggerLevel.AllLoggers;
 
-        CRITICAL: int = ( 1 << 0 );
-        ERROR: int = ( 1 << 1 );
-        WARNING: int = ( 1 << 2 );
-        INFORMATION: int = ( 1 << 3 );
-        DEBUG: int = ( 1 << 4 );
-        TRACE: int = ( 1 << 5 );
-
-    level: int = ( Level.DEBUG | Level.INFORMATION | Level.WARNING | Level.TRACE );
-
-    @staticmethod
-    def ToggleLevel( self, level: Level ):
-
-        if Logger.level & level:
-
-            Logger.level &= level;
-
+    def ToggleLevel( self, level: LoggerLevel ):
+        if self.level & level:
+            self.level &= level;
         else:
+            self.level |= level;
 
-            Logger.level |= level;
+    def SetLevel( self, level: LoggerLevel ):
+        if not self.level & level:
+            self.level |= level;
 
-    @staticmethod
-    def SetLevel( self, level: Level ):
-
-        if not Logger.level & level:
-
-            Logger.level |= level;
-
-    @staticmethod
-    def ClearLevel( self, level: Level ):
-
-        if Logger.level & level:
-
-            Logger.level &= level;
+    def ClearLevel( self, level: LoggerLevel ):
+        if self.level & level:
+            self.level &= level;
 
     def __init__( self, name: str ):
 
@@ -53,10 +63,18 @@ class Logger():
 
     from colorama import Fore as c;
 
-    def log( self, levelname: str, color: str, message: str, level: Level, *args ) -> str:
+    def log( self, levelname: str, color: str, message: str, level: LoggerLevel, *args ) -> str:
 
-        if not Logger.level & level and level != Logger.Level.ERROR and level != Logger.Level.CRITICAL:
-            return;
+        if level != LoggerLevel.Critical or level != LoggerLevel.Error:
+
+            LevelsCheck = ( GlobalLoggerLevel, self.level );
+
+            for LevelCheck in LevelsCheck:
+
+                if LevelCheck != LoggerLevel.AllLoggers:
+
+                    if not LevelCheck & level:
+                        return;
 
         from datetime import datetime;
 
@@ -103,23 +121,23 @@ class Logger():
         print( message );
 
     def debug( self, message: str, *args ):
-        self.log( "debug", self.c.CYAN, message, Logger.Level.DEBUG, *args );
+        self.log( "debug", self.c.CYAN, message, LoggerLevel.Debug, *args );
 
     def info( self, message: str, *args ):
-        self.log( "info", self.c.GREEN, message, Logger.Level.INFORMATION, *args );
+        self.log( "info", self.c.GREEN, message, LoggerLevel.Information, *args );
 
     def warn( self, message: str, *args ):
-        self.log( "warning", self.c.YELLOW, message, Logger.Level.WARNING, *args );
+        self.log( "warning", self.c.YELLOW, message, LoggerLevel.Warning, *args );
 
     def trace( self, message: str, *args ):
-        self.log( "trace", self.c.YELLOW, message, Logger.Level.TRACE, *args );
+        self.log( "trace", self.c.YELLOW, message, LoggerLevel.Trace, *args );
 
     def error( self, message: str, *args, Exit:bool = False ):
-        self.log( "error", self.c.RED, message, Logger.Level.ERROR, *args );
+        self.log( "error", self.c.RED, message, LoggerLevel.Error, *args );
         if Exit:
             exit(1);
 
     def critical( self, message: str, *args, Exit:bool = False ):
-        self.log( "error", self.c.RED, message, Logger.Level.CRITICAL, *args );
+        self.log( "error", self.c.RED, message, LoggerLevel.Critical, *args );
         if Exit:
             exit(1);
