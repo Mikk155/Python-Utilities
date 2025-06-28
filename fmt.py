ecbe940a6797743e47b1920c38ff98de1991113b
Mikk155 <martimsanchez155@gmail.com>
2025-06-28 11:33:04 -0300

    Update scripts headers

--- fmt.py
+++ fmt.py
@@ -1,3 +1,27 @@
+'''
+MIT License
+
+Copyright (c) 2025 Mikk
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE
+'''
+
 class fmt:
 
     from utils.Logger import Logger;
@@ -43,7 +67,7 @@ class fmt:
         return f"<@{user.id}>";
 
     @staticmethod
-    def FormatSourcesWithLicence( licence: str, *, sources_folder: str = None, files: list[str] = None ) -> None:
+    def FormatSourcesWithLicence( licence: str, *, sources_folder: str = None, files: list[str] = [] ) -> None:
         '''
             Formats all .py files including the licence on the header
 
@@ -55,7 +79,7 @@ class fmt:
         '''
 
         from os import walk;
-        from os.path import exists;
+        from os.path import exists, join;
 
         if exists( licence ):
 
@@ -71,7 +95,7 @@ class fmt:
 
             for root, _, DirectoryFiles in walk( sources_folder ):
 
-                ValidFiles = [ file for file in DirectoryFiles if file.endswith( ".py" ) ];
+                ValidFiles = [ join( root, file ) for file in DirectoryFiles if file.endswith( ".py" ) ];
 
                 files += ValidFiles;
 
